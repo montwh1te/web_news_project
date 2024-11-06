cc3818b342529bc94165f1f5f0cb88ec6c7606bf
Eduardo Matuella <eduardomatuella@gmail.com>
2024-11-06 19:27:14 -0300

    add <p> formatiing on modelo.html

--- trendfeeds/tasks.py
+++ trendfeeds/tasks.py
@@ -27,6 +27,9 @@ from trendfeeds.models import Noticias
 from django.template.loader import render_to_string
 #serve para o modelo.html saber oque colocar dentro do bloco, no caso(titulo e conteudo)
 
+from django.utils.safestring import mark_safe
+#marcação para forçar as tags no html em vez de str
+
 import os
 #os.path.join adapta o caminho corretamente.
 
@@ -58,17 +61,15 @@ def coletar_noticias():
 
     processed_urls = set()  
     # Conjunto para armazenar URLs processadas e evitar duplicidade
-
-    try:
-        driver.get(url_principal)  
-        #Acessa o url principal
-
-        for i in range(10):  
-        # Limitar a 10 notícias
     
-            noticias = WebDriverWait(driver, 20).until(
-                # erro no catching CORRIGIR
-                EC.presence_of_all_elements_located((By.CLASS_NAME, 'feed-post-body'))
+    try:
+        
+        driver.get(url_principal)  
+            #Acessa o url principal
+            
+        for i in range(10):
+        
+            noticias = WebDriverWait(driver, 20).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'feed-post-body'))
                 # Esse é um método utilizado com WebDriverWait para aguardar até que um elemento esteja presente no DOM, independentemente de estar visível.
 
                 #Ele é aplicado ao nível do driver (navegador), o que significa que ele procura globalmente na página, sem restrição a um elemento específico.
@@ -89,7 +90,7 @@ def coletar_noticias():
                 #find_element realiza a busca a partir de um elemento pai específico, ou seja, ele só pesquisa dentro do elemento noticia.
 
                 #Isso é útil quando você quer restringir a busca ao escopo de um elemento específico (neste caso, uma notícia específica).
-                print(f"Notícia {i+1} marcada como Tempo Real, pulando...")
+                print(f"Notícia {i + 1} marcada como Tempo Real, pulando...")
                 continue  
                 # Pula essa notícia se for "tempo real"
             except:
@@ -103,27 +104,27 @@ def coletar_noticias():
                 titulo_noticia = noticia.find_element(By.CSS_SELECTOR, 'a.feed-post-link p').text.strip()
                 # Captura o link e o título da notícia
             except:
-                print(f"Erro ao capturar link ou título da notícia {i+1}, pulando...")
+                print(f"Erro ao capturar link ou título da notícia {i + 1}, pulando...")
                 continue
             
 
         
             if "/video" in link_noticia:
                 # Verifica se o link contém
-                print(f"Notícia {i+1} contém algo no link, pulando...")
+                print(f"Notícia {i + 1} contém algo no link, pulando...")
                 continue 
                 # Pula a notícia se o link contiver 
 
             if "/jogo" in link_noticia:
-                print(f"Notícia {i+1} contém algo no link, pulando...")
+                print(f"Notícia {i + 1} contém algo no link, pulando...")
                 continue 
 
             if "/playlist" in link_noticia:
-                print(f"Notícia {i+1} contém algo no link, pulando...")
+                print(f"Notícia {i + 1} contém algo no link, pulando...")
                 continue 
         
             if link_noticia in processed_urls:
-                print(f"Notícia {i+1} já processada, pulando...")
+                print(f"Notícia {i + 1} já processada, pulando...")
                 # Verifica se o link já foi processado
                 continue
             else:
@@ -131,7 +132,7 @@ def coletar_noticias():
                 # Adiciona o link ao conjunto de URLs processadas  
 
 
-            print(f"Notícia {i+1}: {titulo_noticia}")
+            print(f"Notícia {i + 1}: {titulo_noticia}")
             print(f"Link: {link_noticia}")
             # printa o numero da ordem da noticia que ele pegou com o titulo e o link dela
 
@@ -147,7 +148,7 @@ def coletar_noticias():
                 title_text = title_element.text
                 # Pega somente o texto do titulo
             except:
-                print(f"Erro ao capturar o título da notícia {i+1}, pulando...")
+                print(f"Erro ao capturar o título da notícia {i + 1}, pulando...")
                 driver.get(url_principal)
                 time.sleep(2)
                 # Essa linha faz uma pausa fixa de 2 segundos antes de seguir para a próxima linha de código.
@@ -161,7 +162,7 @@ def coletar_noticias():
                 )
                 content_text = content_element.text
             except:
-                print(f"Erro ao capturar o conteudo da notícia {i+1}, pulando...")
+                print(f"Erro ao capturar o conteudo da notícia {i + 1}, pulando...")
                 driver.get(url_principal)
                 time.sleep(2)
                 # Essa linha faz uma pausa fixa de 2 segundos antes de seguir para a próxima linha de código.
@@ -188,19 +189,19 @@ def coletar_noticias():
                 file.write(html_content)
             print(f"Arquivo HTML criado com sucesso: {caminho_arquivo}")
             
-           
+        
             titulo_formatado = re.sub(r'[\\/*?:"<>|]', "", title_text)
             content_text_formatado = formatar_texto(titulo_formatado_semespaco)
 
             # Arquivo HTML final já com o texto transformado
             nome_arquivo = f"{titulo_formatado_semespaco}.html"
+            
             # Define o nome do arquivo HTML e o conteúdo HTML
             caminho_arquivo = os.path.join('trendfeeds/templates/html', nome_arquivo)
             html_content = render_to_string('html/modelo.html', {
                 'title_text': titulo_formatado,
                 'content_text': content_text_formatado
             })
-
 
             with open(caminho_arquivo, "w", encoding="utf-8") as file:
                 file.write(html_content)
@@ -223,7 +224,7 @@ def coletar_noticias():
             driver.get(url_principal)
             time.sleep(2)
             # Retorna à página principal e aguarda o carregamento
-
+                
     finally:
         driver.quit()
         #Fecha todas as janelas e encerra o processo do Chrome iniciado pelo Selenium, liberando os recursos do sistema.
@@ -241,6 +242,9 @@ def formatar_texto(arquivo_nome):
 
     # Remove frases com "+" no início
     text = re.sub(r'\+ [^\n]+', '', text)  
+    
+    paragrafos = __dividir_por_pontos_finais(text, 3)
+    text = __criar_html_com_paragrafos(paragrafos)
     
     # Retorna o texto como valor da função
     return text
@@ -278,6 +282,36 @@ def __captar_tag(file_path):
         text = ""
         print("Não foi encontrada nenhuma tag <p> no conteúdo.")
     return text
+    
+def __dividir_por_pontos_finais(texto, num_pontos_finais):
+    # Divide o texto com base nos pontos finais
+    sentencas = texto.split('.')
+    paragrafo_atual = ""
+    paragrafos = []
+    
+    for i in range(0, len(sentencas), num_pontos_finais):
+        # Adiciona num_pontos sentenças ao parágrafo atual, mas verifica o limite da lista
+        paragrafo_atual = '. '.join(sentencas[i:i + num_pontos_finais]).strip() + '.'
+        
+        # Adiciona o parágrafo ao resultado final, se não estiver vazio
+        if paragrafo_atual.strip():
+            paragrafos.append(paragrafo_atual)
+        paragrafo_atual = ""  # Reseta para o próximo parágrafo
+    
+    return paragrafos
+
+def __criar_html_com_paragrafos(paragrafos):
+    
+    html_content = ""
+    
+    # Cria as tags <p> para cada parágrafo
+    for paragrafo in paragrafos:
+        html_content += f"<p class='noticia-conteudo'>{paragrafo}</p>\n"
+        
+    # Quando for retornar ou renderizar o HTML no Django
+    html_content = mark_safe(html_content) 
+    
+    return html_content
     
 def iniciar_scheduler():
     pass
