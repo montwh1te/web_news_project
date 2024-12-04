4f893e21275103674b973fa28d80e173f32b6212
Eduardo Paim <zeedu223@gmail.com>
2024-12-04 11:27:16 -0300

    criacao dos css

--- web_news/urls.py
+++ web_news/urls.py
@@ -3,9 +3,14 @@ from django.conf.urls.static import static
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('trendfeeds.urls')),  # Inclui as rotas do app locadora
-    path('', include('users.urls')),  # Inclui as rotas do app users
-    path('api/', include('api.urls')) # Inclui as rotas do app api
+    # Inclui as rotas do app trendfeeds
+    path('', include('trendfeeds.urls')), 
+
+    # Inclui as rotas do app users
+    path('', include('users.urls')), 
+
+    # Inclui as rotas do app api 
+    path('api/', include('api.urls')) 
 ]
 
 # Adiciona URLs para arquivos de mídia em desenvolvimento
